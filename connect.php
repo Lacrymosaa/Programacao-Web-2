cb27d69cb68f2f924426f90bc3a3d90c78b10ee0
Lapis <82874011+DiabolicWitch@users.noreply.github.com>
2023-03-20 11:59:31 -0300

    Add files via upload
    Connect feito em aula (INCOMPLETO)

---
+++ connect.php
@@ -0,0 +1,40 @@
+<?php
+    class Database {
+        private $host = "localhost";
+        private $username = "username";
+        private $password = "password";
+        private $database = "database_name";
+        private $pdo;
+    
+        public function __construct() {
+            try {
+                $this->pdo = new PDO("mysql:host={$this->host};dbname={$this->database}", $this->username, $this->password);
+                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            } catch (PDOException $e) {
+                die("Failed to connect to database: " . $e->getMessage());
+            }
+        }
+    
+        public function executeQuery($query, $params = array()) {
+            try {
+                $statement = $this->pdo->prepare($query);
+                $statement->execute($params);
+                return $statement;
+            } catch (PDOException $e) {
+                die("Failed to execute query: " . $e->getMessage());
+            }
+        }
+
+        public function getEstadoConexao() {
+            if ($this->pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS) == 'Connection OK') {
+                return "Conexão bem-sucedida.";
+            } else {
+                return "Falha na conexão.";
+            }
+        }
+        
+        
+    }
+    
+?>
